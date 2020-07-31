f2c0e6bcd31b2c6141c7910ebc4a9b97abbb7a68
Mats Büscher <mats.buescher@gmx.de>
2020-07-31 15:31:18 +0200

    Added exceptions and SQL procedures

--- Version1.0/Form1.cs
+++ Version1.0/Form1.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.Azure.Storage;
+﻿using Dapper;
+using Microsoft.Azure.Storage;
 using Microsoft.Azure.Storage.Blob;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -25,6 +28,85 @@ namespace Version1._0
 
         private void button1_Click(object sender, EventArgs e)
         {
+            CheckUpdate();
+        }
+
+        public async void CheckUpdate()
+        {
+            bool retry;
+
+            do
+            {
+                retry = false;
+                int b = 3;
+                await Task.Run(() =>
+                {
+                    try
+                    {
+                        using (var conn = new SqlConnection("Server = tcp:zambu.database.windows.net,1433; Initial Catalog = ZAMBU_SQL; Persist Security Info = False; User ID = Mats; Password =ZambuYT#2290; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;"))
+                        {
+                            var parameters = new DynamicParameters();
+                            parameters.Add("@foo", dbType: DbType.Int32, direction: ParameterDirection.ReturnValue);
+                            parameters.Add("@Version", "1.0");
+                            parameters.Add("@Product", "Test");
+                            conn.Execute("[dbo].spCheckVersion", parameters, commandType: CommandType.StoredProcedure);
+
+                            b = parameters.Get<int>("@foo");
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        if(MessageBox.Show("Threre was an error connecting to the SQL server and check for updates.\n Do you want to retry?", "SQL connection error!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            retry = true;
+                        }
+                    }
+                });
+
+                if (b == 0)
+                {
+                    MessageBox.Show("You have the latest version");
+                }
+                else
+                {
+                    if (MessageBox.Show("This version is of the application is old.\nInitialize update?", "You have an old version!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        try
+                        {
+                            await Task.Run(() =>
+                            {
+                                string newName;
+                                using (var conn = new SqlConnection("Server = tcp:zambu.database.windows.net,1433; Initial Catalog = ZAMBU_SQL; Persist Security Info = False; User ID = Mats; Password =ZambuYT#2290; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;"))
+                                {
+                                    DynamicParameters _param = new DynamicParameters();
+                                    _param.Add("@Product", "test");
+                                    _param.Add("@newName", "", DbType.String, ParameterDirection.Output);
+
+                                    conn.Execute("[dbo].spGetNewVersion @Product, @newName output", _param);
+
+                                    newName = _param.Get<string>("newName");
+                                }
+                                update(newName);
+                            });
+                        }
+                        catch (Exception)
+                        {
+                            if(MessageBox.Show("Threre was an error connecting to the SQL server and check for updates.\n Do you want to retry?", "SQL connection error!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                            {
+                                retry = true;
+                            }
+                        }
+
+                    }
+                }
+            } while (retry);
+            
+        }
+
+        private void update(string newVersion)
+        {
+
             UpdateScreen updateScreen = new UpdateScreen();
             updateScreen.Show();
 
@@ -33,12 +115,27 @@ namespace Version1._0
             string path = Application.StartupPath;
             string filename = Path.GetFileName(Application.ExecutablePath);
             string PID = Process.GetCurrentProcess().Id.ToString();
-            string newFilename = "Version1.1.exe";
-            AzureFileDownload("Updater.exe", "updates", path);
+            string newFilename = newVersion;
+            bool retry;
+            do
+            {
+                retry = false;
+                try
+                {
+                    AzureFileDownload("Updater.exe", "updates", path);
 
-            Process.Start("Updater.exe", $" \"{path}\" \"{filename}\" {PID} \"{newFilename}\"");
+                    Process.Start("Updater.exe", $" \"{path}\" \"{filename}\" {PID} \"{newFilename}\"");
+                }
+                catch (Exception)
+                {
+                    if (MessageBox.Show("An error occured while downloading the Updater.\n do you want to retry?", "Critical error!!!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        retry = true;
+                    }
+                    
+                }
+            } while (retry);
         }
-
 
         public static void AzureFileDownload(string fileName, string containerName, string path)
         {
